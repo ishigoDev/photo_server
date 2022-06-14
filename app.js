3a47c6b3f1a75dcd94c046426fe4128f7c98365c
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-14 22:45:55 +0530

    resolving-cors-v7

--- app.js
+++ app.js
@@ -8,19 +8,18 @@ import admin_quote_router from './controllers/adminQuotes.js';
 
 
 const app = express();
-app.use(cors({origin:'*'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('public'));  
 env.config();
 //cors error solution
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin','https://neutrography.web.app/');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-//     res.setHeader('Access-Control-Allow-Credentials', true);    
-//     next();
-// });
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+    res.setHeader('Access-Control-Allow-Credentials', true);    
+    next();
+});
 
     
 app.use('/neutro',router);
