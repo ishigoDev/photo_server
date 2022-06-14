9f14363b7b225abec924cddd0f89eda9e8f90c1b
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-14 22:26:28 +0530

    resolving-cors-v2

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ env.config();
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     res.setHeader('Access-Control-Allow-Credentials', true);    
     next();
 });
