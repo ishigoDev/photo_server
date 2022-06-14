ae8334d4f81543e060ef893d092ed54768301d61
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-14 22:31:36 +0530

    resolving-cors-v3

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 env.config();
 //cors error solution
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin','https://neutrography.web.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
     res.setHeader('Access-Control-Allow-Credentials', true);    
