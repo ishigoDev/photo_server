bb9d54bcd1017117e704109b6f219b34cd07ac88
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-15 22:36:00 +0530

    changing-email-v4

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -4,8 +4,8 @@ env.config();
 let transporter = nodemailer.createTransport({    
         service: 'gmail',
         auth: {        
-          user: "evincedevapi@gmail.com",
-          pass: "OldEvince12#",        
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,        
         }      
 });
 
