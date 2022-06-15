4adabae208468a951d43bfd670125b23f01b46d8
ishigoDev <104483018+pranaySpark18@users.noreply.github.com>
2022-06-15 22:45:04 +0530

    changing-email-v5

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -3,6 +3,7 @@ import env from 'dotenv';
 env.config();
 let transporter = nodemailer.createTransport({    
         service: 'gmail',
+        host: 'smtp.gmail.com',
         auth: {        
           user: process.env.MAIL_USERNAME,
           pass: process.env.MAIL_PASSWORD,        
